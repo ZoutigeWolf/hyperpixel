2ba6cefb410db2db7be32fc39240a2369f4cba0b
ZoutigeWolf <guuskamphuis@gmail.com>
2023-09-14 12:14:35 +0200

    init

---
+++ main.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+import os
+import sys
+import signal
+import pygame
+from pygame import gfxdraw
+import math
+import time
+import datetime
+from colorsys import hsv_to_rgb
+from hyperpixel2r import Touch
+
+
+"""
+Run with: sudo SDL_FBDEV=/dev/fb0 python3 clock.py
+"""
+
+
+class Hyperpixel2r:
+    screen = None
+    def __init__(self):
+        self._init_display()
+
+        self.screen.fill((0, 0, 0))
+        if self._rawfb:
+            self._updatefb()
+        else:
+            pygame.display.update()
+
+        # For some reason the canvas needs a 7px vertical offset
+        # circular screens are weird...
+        self.center = (240, 247)
+
+        self._running = False
+
+    def _exit(self, sig, frame):
+        self._running = False
+        print("\nExiting!...\n")
+
+    def _init_display(self):
+        self._rawfb = False
+        # Based on "Python GUI in Linux frame buffer"
+        # http://www.karoltomala.com/blog/?p=679
+        DISPLAY = os.getenv("DISPLAY")
+        if DISPLAY:
+            print("Display: {0}".format(DISPLAY))
+
+        if os.getenv('SDL_VIDEODRIVER'):
+            print("Using driver specified by SDL_VIDEODRIVER: {}".format(os.getenv('SDL_VIDEODRIVER')))
+            pygame.display.init()
+            size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+            if size == (480, 480): # Fix for 480x480 mode offset
+                size = (640, 480)
+            self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.NOFRAME | pygame.HWSURFACE)
+            return
+
+        else:
+            # Iterate through drivers and attempt to init/set_mode
+            for driver in ['rpi', 'kmsdrm', 'fbcon', 'directfb', 'svgalib']:
+                os.putenv('SDL_VIDEODRIVER', driver)
+                try:
+                    pygame.display.init()
+                    size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+                    if size == (480, 480):  # Fix for 480x480 mode offset
+                        size = (640, 480)
+                    self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.NOFRAME | pygame.HWSURFACE)
+                    print("Using driver: {0}, Framebuffer size: {1:d} x {2:d}".format(driver, *size))
+                    return
+                except pygame.error as e:
+                    print('Driver "{0}" failed: {1}'.format(driver, e))
+                    continue
+                break
+
+        print("All SDL drivers failed, falling back to raw framebuffer access.")
+        self._rawfb = True
+        os.putenv('SDL_VIDEODRIVER', 'dummy')
+        pygame.display.init()  # Need to init for .convert() to work
+        self.screen = pygame.Surface((480, 480))
+
+    def __del__(self):
+        "Destructor to make sure pygame shuts down, etc."
+
+    def touch(self, x, y, state):
+        # Handle touch
+        pass
+
+    def _updatefb(self):
+        fbdev = os.getenv('SDL_FBDEV', '/dev/fb0')
+        with open(fbdev, 'wb') as fb:
+            fb.write(self.screen.convert(16, 0).get_buffer())
+
+    def run(self):
+        self._running = True
+        signal.signal(signal.SIGINT, self._exit)
+        while self._running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+                    break
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self._running = False
+                        break
+
+            # code
+            gfxdraw.circle(self.screen, self.center[0], self.center[1], 50, (255, 0, 255))
+
+            if self._rawfb:
+                self._updatefb()
+            else:
+                pygame.display.flip()
+            self._clock.tick(30)  # Aim for 30fps
+
+        pygame.quit()
+        sys.exit(0)
+
+
+display = Hyperpixel2r()
+touch = Touch()
+
+
+@touch.on_touch
+def handle_touch(touch_id, x, y, state):
+    display.touch(x, y, state)
+
+
+display.run()
